8dc51fb771930e1793929d2fc22e054962ee7ca4
gtobio11 <gtobio11@gmail.com>
2018-06-11 11:49:12 +0900

    암호화 복호화 파일 작성완료, 테스팅 필요

---
+++ decrypt.py
@@ -0,0 +1,32 @@
+import os, fnmatch
+
+
+def search_file(dirname):
+    for dirpath, dirs, files in os.walk(dirname):
+        if "Windows" not in dirpath:
+            for fileName in files:
+                if fnmatch.fnmatch(fileName, '*.ctc'):
+                    try:
+                        decrypt(os.path.join(dirpath, fileName))
+                    except:
+                        pass
+
+
+def decrypt(file):
+    with open(file, 'rb') as F:
+        with open(file[:-4], 'wb') as F1:
+            binStr = F.read()
+
+            encStr = b''
+
+            while i < len(binStr):
+                data = (binStr[i] * 256) + binStr[i + 1]
+                temp = ((data ** 6029) % 10403).to_bytes(1, byteorder='big')
+                encStr += temp
+                i = i + 2
+
+            F1.write(encStr)
+    os.remove(file)
+
+
+search_file("C:/")
